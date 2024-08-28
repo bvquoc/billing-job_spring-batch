3a08a5179513ab500c18b32dca0333a6127e4079
Quoc Bui (Martin) <quoc.bui@navercorp.com>
2024-08-28 10:45:16 +0700

    add config

---
+++ src/main/java/naver/martin/billingjob/BillingJobConfiguration.java
@@ -0,0 +1,8 @@
+package naver.martin.billingjob;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BillingJobConfiguration {
+    // TODO: add job definition here
+}
